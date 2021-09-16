94d9563efeaa6857395ebe61165c74b91b3f43bf
Peace Munyai <sapeace1@gmail.com>
2021-09-16 20:08:46 +0100

    added ads and share job post details as text

--- app/src/main/java/com/theecoder/sajobsearch/MainActivity.kt
+++ app/src/main/java/com/theecoder/sajobsearch/MainActivity.kt
@@ -3,19 +3,28 @@ package com.theecoder.sajobsearch
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
+import com.google.android.gms.ads.*
+import com.google.android.gms.ads.interstitial.InterstitialAd
+import com.google.android.gms.ads.interstitial.InterstitialAdLoadCallback
 
 class MainActivity : AppCompatActivity() {
+    lateinit var mAdView : AdView
+    private var mInterstitialAd: InterstitialAd? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
          setTheme(R.style.Main_Theme)
         setContentView(R.layout.activity_main)
 
+        loadAdBanner()
+        loadAdInterstatial()
+
         var session = intent.getStringExtra("login")
         if(session == "yes"){
             findViewById<Button>(R.id.search_button).setOnClickListener{
-                val i = Intent(this, SearchActivity::class.java)
-                startActivity(i)
+                showInterAd()
             }
         }else{
             val i = Intent(this, LoginActivity::class.java)
@@ -24,5 +33,53 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    private fun showInterAd() {
+        if(mInterstitialAd != null){
+            mInterstitialAd?.fullScreenContentCallback = object: FullScreenContentCallback(){
+                override fun onAdDismissedFullScreenContent() {
+                    Log.d("adView", "Ad was dismissed.")
+                    val i = Intent(this@MainActivity, SearchActivity::class.java)
+                    startActivity(i)
+                }
+
+                override fun onAdFailedToShowFullScreenContent(adError: AdError?) {
+                    Log.d("adView", "Ad failed to show.")
+
+                }
+
+                override fun onAdShowedFullScreenContent() {
+                    Log.d("adView", "Ad showed fullscreen content.")
+                    mInterstitialAd = null
+                }
+
+            }
+
+            mInterstitialAd?.show(this@MainActivity)
+        }else{
+            val i = Intent(this@MainActivity, SearchActivity::class.java)
+            startActivity(i)
+        }
+    }
+
+    private fun loadAdBanner()
+    {
+        MobileAds.initialize(this) {}
+        mAdView = findViewById(R.id.adView)
+        val adRequest = AdRequest.Builder().build()
+        mAdView.loadAd(adRequest)
+    }
+    private fun loadAdInterstatial(){
+        var adRequest = AdRequest.Builder().build()
+
+        InterstitialAd.load(this,"ca-app-pub-3940256099942544/1033173712", adRequest, object : InterstitialAdLoadCallback() {
+            override fun onAdFailedToLoad(adError: LoadAdError) {
+                mInterstitialAd = null
+            }
+
+            override fun onAdLoaded(interstitialAd: InterstitialAd) {
+                mInterstitialAd = interstitialAd
+            }
+        })
+    }
 
 }
